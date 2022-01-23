c8df3e7b6dc256be0237f413a921de164e9cb5e8
James Wilson <jw@jameswilson.io>
2022-01-23 11:25:45 +1100

    - Added test that demonstrates failure to URL-encode userinfo in DSN
    Signed-off-by: James Wilson <jw@jameswilson.io>

--- pkg/clients/postgresql/postgresql_test.go
+++ pkg/clients/postgresql/postgresql_test.go
@@ -9,9 +9,10 @@ func TestDSNURLEscaping(t *testing.T) {
 	port := "5432"
 	db := "postgres"
 	user := "username"
-	pass := "password"
-	dsn := DSN(user, pass, endpoint, port, db)
-	if dsn != "postgres://"+user+":"+pass+"@"+endpoint+":"+port+"/"+db {
+	rawPass := "password^"
+	encPass := "password%5E"
+	dsn := DSN(user, rawPass, endpoint, port, db)
+	if dsn != "postgres://"+user+":"+encPass+"@"+endpoint+":"+port+"/"+db {
 		t.Errorf("DSN string did not match expected output with userinfo URL encoded")
 	}
 }
