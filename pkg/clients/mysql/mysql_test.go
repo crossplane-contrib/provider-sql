b1ad2d8e10b9855f5f5b4589378e421a108089ef
Matt Mencel <matt@techminer.net>
2022-04-12 09:31:20 -0500

    cleanup and make reviewable - add Contributing docs to README
    Signed-off-by: Matt Mencel <matt@techminer.net>

--- pkg/clients/mysql/mysql_test.go
+++ pkg/clients/mysql/mysql_test.go
@@ -14,11 +14,11 @@ func TestDSNURLEscaping(t *testing.T) {
 	tls := "true"
 	dsn := DSN(user, rawPass, endpoint, port, tls)
 	if dsn != fmt.Sprintf("%s:%s@tcp(%s:%s)/?tls=%s",
-					user,
-					encPass,
-					endpoint,
-					port,
-					tls) {
-					t.Errorf("DSN string did not match expected output with URL encoded")
+		user,
+		encPass,
+		endpoint,
+		port,
+		tls) {
+		t.Errorf("DSN string did not match expected output with URL encoded")
 	}
 }
