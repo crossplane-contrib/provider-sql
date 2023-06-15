afeb504689839f624c47a0d237729e22ec307d16
Adrian Tanko <adrian.tanko@sap.com>
2023-06-15 04:55:18 +0200

    add hana client tests

---
+++ pkg/clients/hana/hana_test.go
@@ -0,0 +1,112 @@
+package hana
+
+import (
+	"context"
+	"errors"
+	"github.com/crossplane-contrib/provider-sql/pkg/clients/xsql"
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+var hanaDb = hanaDB{
+	dsn:      "hdb://<USER>:<PASSWORD>@<HOST>:<PORT>?TLSServerName=<HOST>",
+	endpoint: "<HOST>",
+	port:     "<PORT>",
+}
+
+func TestNew(t *testing.T) {
+	type args struct {
+		creds map[string][]byte
+	}
+
+	type want struct {
+		db xsql.DB
+	}
+
+	cases := map[string]struct {
+		reason string
+		args   args
+		want   want
+	}{
+		"validCreds": {
+			reason: "valid Credentials",
+			args: args{
+				creds: map[string][]byte{
+					xpv1.ResourceCredentialsSecretEndpointKey: []byte("endpoint.hanacloud.ondemand.com"),
+					xpv1.ResourceCredentialsSecretPortKey:     []byte("443"),
+					xpv1.ResourceCredentialsSecretUserKey:     []byte("example-user"),
+					xpv1.ResourceCredentialsSecretPasswordKey: []byte("Secur3Pass0rd"),
+				},
+			},
+			want: want{
+				db: hanaDB{
+					dsn:      "hdb://example-user:Secur3Pass0rd@endpoint.hanacloud.ondemand.com:443?TLSServerName=endpoint.hanacloud.ondemand.com",
+					endpoint: "endpoint.hanacloud.ondemand.com",
+					port:     "443",
+				},
+			},
+		},
+		//TODO Add more test cases
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			db := New(tc.args.creds)
+
+			if diff := cmp.Diff(tc.want.db, db, cmp.AllowUnexported(hanaDB{})); diff != "" {
+				t.Errorf("\n%s\ne.New(...): -want hanaDB, +got error:\n%s\n", tc.reason, diff)
+			}
+		})
+	}
+}
+
+func TestExec(t *testing.T) {
+	type args struct {
+		query xsql.Query
+	}
+
+	type want struct {
+		error error
+	}
+
+	cases := map[string]struct {
+		reason string
+		args   args
+		want   want
+	}{
+		"validQuery": {
+			reason: "valid query",
+			args: args{
+				query: xsql.Query{
+					String: "SELECT * FROM DUMMY",
+				},
+			},
+			want: want{
+				error: nil,
+			},
+		},
+		"invalidQuery": {
+			reason: "invalid query",
+			args: args{
+				query: xsql.Query{
+					String: "INVALID SQL QUERY",
+				},
+			},
+			want: want{
+				error: errors.New("error"),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			err := hanaDb.Exec(context.Background(), tc.args.query)
+
+			if (err != nil && tc.want.error == nil) || (err == nil && tc.want.error != nil) {
+				t.Errorf("\n%s\ne.New(...): -want error, +got error:\n%s\n", tc.reason, err)
+			}
+
+		})
+	}
+}
