40eec0ffc2c3d5b0fc9541eee4ffe8f5e78da7c1
Adrian Tanko <adrian.tanko@sap.com>
2023-06-16 11:20:37 +0200

    feat: add hana user type

---
+++ apis/hana/v1alpha1/user_types.go
@@ -0,0 +1,86 @@
+/*
+Copyright 2022 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"reflect"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+)
+
+// UserParameters are the configurable fields of a User.
+type UserParameters struct {
+	ConfigurableField string `json:"configurableField"`
+}
+
+// UserObservation are the observable fields of a User.
+type UserObservation struct {
+	ObservableField string `json:"observableField,omitempty"`
+}
+
+// A UserSpec defines the desired state of a User.
+type UserSpec struct {
+	xpv1.ResourceSpec `json:",inline"`
+	ForProvider       UserParameters `json:"forProvider"`
+}
+
+// A UserStatus represents the observed state of a User.
+type UserStatus struct {
+	xpv1.ResourceStatus `json:",inline"`
+	AtProvider          UserObservation `json:"atProvider,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// A User is an example API type.
+// +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
+// +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
+// +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,sql}
+type User struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   UserSpec   `json:"spec"`
+	Status UserStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// UserList contains a list of User
+type UserList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []User `json:"items"`
+}
+
+// User type metadata.
+var (
+	UserKind             = reflect.TypeOf(User{}).Name()
+	UserGroupKind        = schema.GroupKind{Group: Group, Kind: UserKind}.String()
+	UserKindAPIVersion   = UserKind + "." + SchemeGroupVersion.String()
+	UserGroupVersionKind = SchemeGroupVersion.WithKind(UserKind)
+)
+
+func init() {
+	SchemeBuilder.Register(&User{}, &UserList{})
+}
--- apis/hana/v1alpha1/zz_generated.managedlist.go
+++ apis/hana/v1alpha1/zz_generated.managedlist.go
@@ -27,3 +27,12 @@ func (l *DbSchemaList) GetItems() []resource.Managed {
 	}
 	return items
 }
+
+// GetItems of this UserList.
+func (l *UserList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
