389234e8af67ca98901d7991ed5cdd8f98d5681d
Adrian Tanko <adrian.tanko@sap.com>
2023-07-03 11:18:12 +0200

    feat: moving usergroup queries to client

--- pkg/controller/hana/usergroup/usergroup_controller.go
+++ pkg/controller/hana/usergroup/usergroup_controller.go
@@ -19,21 +19,17 @@ package usergroup
 import (
 	"context"
 	"fmt"
-	"github.com/crossplane-contrib/provider-sql/pkg/clients/hana"
-	"github.com/crossplane-contrib/provider-sql/pkg/clients/xsql"
+	"github.com/crossplane-contrib/provider-sql/pkg/clients/hana/usergroup"
 	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
-	corev1 "k8s.io/api/core/v1"
-	"strings"
-
-	"github.com/pkg/errors"
-	"k8s.io/apimachinery/pkg/types"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
 	"github.com/crossplane/crossplane-runtime/pkg/controller"
 	"github.com/crossplane/crossplane-runtime/pkg/event"
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/crossplane-contrib/provider-sql/apis/hana/v1alpha1"
 	apisv1alpha1 "github.com/crossplane-contrib/provider-sql/apis/hana/v1alpha1"
@@ -45,10 +41,7 @@ const (
 	errGetPC        = "cannot get ProviderConfig"
 	errNoSecretRef  = "ProviderConfig does not reference a credentials Secret"
 
-	errGetSecret       = "cannot get credentials Secret"
-	errSelectUsergroup = "cannot select Usergroup"
-	errCreateUsergroup = "cannot create Usergroup"
-	errDropUsergroup   = "cannot drop Usergroup"
+	errGetSecret = "cannot get credentials Secret"
 )
 
 // A NoOpService does nothing.
@@ -65,7 +58,7 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 	t := resource.NewProviderConfigUsageTracker(mgr.GetClient(), &v1alpha1.ProviderConfigUsage{})
 	r := managed.NewReconciler(mgr,
 		resource.ManagedKind(v1alpha1.UsergroupGroupVersionKind),
-		managed.WithExternalConnecter(&connector{kube: mgr.GetClient(), usage: t, newDB: hana.New}),
+		managed.WithExternalConnecter(&connector{kube: mgr.GetClient(), usage: t, newClient: usergroup.New}),
 		managed.WithLogger(o.Logger.WithValues("controller", name)),
 		managed.WithPollInterval(o.PollInterval),
 		managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))))
@@ -79,9 +72,9 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 // A connector is expected to produce an ExternalClient when its Connect method
 // is called.
 type connector struct {
-	kube  client.Client
-	usage resource.Tracker
-	newDB func(creds map[string][]byte) xsql.DB
+	kube      client.Client
+	usage     resource.Tracker
+	newClient func(creds map[string][]byte) usergroup.Client
 }
 
 // Connect typically produces an ExternalClient by:
@@ -115,16 +108,16 @@ func (c *connector) Connect(ctx context.Context, mg resource.Managed) (managed.E
 	}
 
 	return &external{
-		db:   c.newDB(s.Data),
-		kube: c.kube,
+		client: c.newClient(s.Data),
+		kube:   c.kube,
 	}, nil
 }
 
 // An ExternalClient observes, then either creates, updates, or deletes an
 // external resource to ensure it reflects the managed resource's desired state.
 type external struct {
-	db   xsql.DB
-	kube client.Client
+	client usergroup.Client
+	kube   client.Client
 }
 
 func (c *external) Observe(ctx context.Context, mg resource.Managed) (managed.ExternalObservation, error) {
@@ -133,56 +126,19 @@ func (c *external) Observe(ctx context.Context, mg resource.Managed) (managed.Ex
 		return managed.ExternalObservation{}, errors.New(errNotUsergroup)
 	}
 
-	// These fmt statements should be removed in the real implementation.
-	fmt.Printf("Observing: %+v", cr)
-
-	observed := &v1alpha1.UsergroupObservation{
-		UsergroupName:    "",
-		DisableUserAdmin: false,
-		Parameters:       make(map[string]string),
+	parameters := &v1alpha1.UsergroupParameters{
+		UsergroupName: cr.Spec.ForProvider.UsergroupName,
+		Parameters:    make(map[string]string),
 	}
 
-	usergroupName := strings.ToUpper(cr.Spec.ForProvider.UsergroupName)
+	extObservation, err := c.client.Observe(ctx, parameters)
 
-	query := "SELECT USERGROUP_NAME, IS_USER_ADMIN_ENABLED FROM SYS.USERGROUPS WHERE USERGROUP_NAME = ?"
-
-	err := c.db.Scan(ctx, xsql.Query{String: query, Parameters: []interface{}{usergroupName}}, &observed.UsergroupName, &observed.DisableUserAdmin)
-	if xsql.IsNoRows(err) {
-		return managed.ExternalObservation{ResourceExists: false}, nil
-	}
-	if err != nil {
-		return managed.ExternalObservation{}, errors.Wrap(err, errSelectUsergroup)
+	if err == nil {
+		cr.SetConditions(xpv1.Available())
 	}
 
-	queryParams := "SELECT USERGROUP_NAME, PARAMETER_NAME, PARAMETER_VALUE FROM SYS.USERGROUP_PARAMETERS WHERE USERGROUP_NAME = ?"
+	return extObservation, err
 
-	rows, err := c.db.Query(ctx, xsql.Query{String: queryParams, Parameters: []interface{}{usergroupName}})
-
-	for rows.Next() {
-		var name, parameter, value string
-		rowErr := rows.Scan(&name, &parameter, &value)
-		if rowErr == nil {
-			observed.Parameters[parameter] = value
-		}
-	}
-
-	cr.SetConditions(xpv1.Available())
-
-	return managed.ExternalObservation{
-		// Return false when the external resource does not exist. This lets
-		// the managed resource reconciler know that it needs to call Create to
-		// (re)create the resource, or that it has successfully been deleted.
-		ResourceExists: true,
-
-		// Return false when the external resource exists, but it not up to date
-		// with the desired managed resource state. This lets the managed
-		// resource reconciler know that it needs to call Update.
-		ResourceUpToDate: true,
-
-		// Return any details that may be required to connect to the external
-		// resource. These will be stored as the connection secret.
-		ConnectionDetails: managed.ConnectionDetails{},
-	}, nil
 }
 
 func (c *external) Create(ctx context.Context, mg resource.Managed) (managed.ExternalCreation, error) {
@@ -202,54 +158,18 @@ func (c *external) Create(ctx context.Context, mg resource.Managed) (managed.Ext
 		EnableParameterSet: cr.Spec.ForProvider.EnableParameterSet,
 	}
 
-	query := fmt.Sprintf("CREATE USERGROUP %s", parameters.UsergroupName)
+	cr.SetConditions(xpv1.Creating())
 
-	if parameters.DisableUserAdmin {
-		query += " DISABLE USER ADMIN"
-	}
+	extCreation, err := c.client.Create(ctx, parameters)
 
-	if parameters.NoGrantToCreator {
-		query += " NO GRANT TO CREATOR"
-	}
-
-	if len(parameters.Parameters) > 0 {
-		query += " SET PARAMETER"
-		for key, value := range parameters.Parameters {
-			query += fmt.Sprintf(" '%s' = '%s',", key, value)
-		}
-		query = strings.TrimSuffix(query, ",")
-	}
-
-	if parameters.EnableParameterSet != "" {
-		query += fmt.Sprintf(" ENABLE PARAMETER SET '%s'", parameters.EnableParameterSet)
-	}
-
-	err := c.db.Exec(ctx, xsql.Query{String: query})
-
-	if err != nil {
-		return managed.ExternalCreation{}, errors.Wrap(err, errCreateUsergroup)
-	}
-
-	return managed.ExternalCreation{
-		// Optionally return any details that may be required to connect to the
-		// external resource. These will be stored as the connection secret.
-		ConnectionDetails: managed.ConnectionDetails{},
-	}, nil
+	return extCreation, err
 }
 
 func (c *external) Update(ctx context.Context, mg resource.Managed) (managed.ExternalUpdate, error) {
-	cr, ok := mg.(*v1alpha1.Usergroup)
-	if !ok {
-		return managed.ExternalUpdate{}, errors.New(errNotUsergroup)
-	}
 
-	fmt.Printf("Updating: %+v", cr)
+	//TODO
 
-	return managed.ExternalUpdate{
-		// Optionally return any details that may be required to connect to the
-		// external resource. These will be stored as the connection secret.
-		ConnectionDetails: managed.ConnectionDetails{},
-	}, nil
+	return managed.ExternalUpdate{}, nil
 }
 
 func (c *external) Delete(ctx context.Context, mg resource.Managed) error {
@@ -258,16 +178,13 @@ func (c *external) Delete(ctx context.Context, mg resource.Managed) error {
 		return errors.New(errNotUsergroup)
 	}
 
-	fmt.Printf("Deleting: %+v", cr)
-	cr.SetConditions(xpv1.Deleting())
-
-	query := fmt.Sprintf("DROP USERGROUP %s", cr.Spec.ForProvider.UsergroupName)
-
-	err := c.db.Exec(ctx, xsql.Query{String: query})
-
-	if err != nil {
-		return errors.Wrap(err, errDropUsergroup)
+	parameters := &v1alpha1.UsergroupParameters{
+		UsergroupName: cr.Spec.ForProvider.UsergroupName,
 	}
 
-	return nil
+	cr.SetConditions(xpv1.Deleting())
+
+	err := c.client.Delete(ctx, parameters)
+
+	return err
 }
